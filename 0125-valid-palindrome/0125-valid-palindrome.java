4ffc1ec1d4867473d96e7b1bcaa0aff6b1cd65e2
Akarshit Srivastava <161750326+neuralakarshit@users.noreply.github.com>
2024-08-12 10:07:10 +0530

    Time: 14 ms (34.78%), Space: 44.8 MB (34.42%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -4,28 +4,18 @@ class Solution {
 
 
         String s1=s.replaceAll("[^A-Za-z0-9]","").toLowerCase();
-        String modified="";
+        int l=s1.length();
         boolean f=false;
 
         if(s1.length()==1||s1.equals(""))
         {
             return true;
         }
-        
-        
-        for(int i=0;i<s1.length();i++)
-        {
-           if(!(s1.charAt(i)==' '))
-           {
-           modified+=s1.charAt(i);
-           }
-        }
-        int l=modified.length();
 
         int i=0;
-        while(i<l/2)
+        while(i<=l/2)
         {
-            if(modified.charAt(i)==modified.charAt(l-i-1))
+            if(s1.charAt(i)==s1.charAt(l-i-1))
             {
                 f=true;
             }
