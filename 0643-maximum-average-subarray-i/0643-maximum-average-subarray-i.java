a0b5237f6848906ed702dfe6a65d5ca04fb8312e
Akarshit Srivastava <161750326+neuralakarshit@users.noreply.github.com>
2024-08-16 01:14:58 +0530

    Time: 3 ms (81.09%), Space: 56.5 MB (17.59%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -1,3 +1,27 @@
+/* NOTE--> DEFAULT APPROACH!
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        int sum = 0;
+
+        // Calculate the sum of the first k elements
+        for (int i = 0; i < k; i++) {
+            sum += nums[i];
+        }
+
+        int max = sum;
+
+        // Slide the window across the array
+        for (int i = k; i < nums.length; i++) {
+            sum = sum + nums[i] - nums[i - k];
+            max = Math.max(max, sum);
+        }
+
+        // Return the maximum average
+        return (double) max / k;
+    }
+}
+
+*/
 class Solution {
     public double findMaxAverage(int[] nums, int k) 
     
@@ -12,13 +36,13 @@ class Solution {
         {
                 if(c<k)
                 { 
-                    sum=sum+nums[i];
+                    sum=sum+nums[i]; // first four elements sum
                     c++;               
                     max=sum;    
                 }
                 else
-                {
-                    sum=sum+(nums[i]-nums[i-k]);
+                {                                
+                    sum=sum+(nums[i]-nums[i-k]); // nextsum - subtracting 1,2,3..elem. frmstart
                     max=Math.max(max,sum);       
                 }
         }
