aebab83df67a63d997fff1006ca07e7ed0f30977
Akarshit Srivastava <161750326+neuralakarshit@users.noreply.github.com>
2024-08-17 00:34:53 +0530

    Time: 15 ms (57.16%), Space: 45.3 MB (9.46%) - LeetHub

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,38 +1,45 @@
-class Solution
- {
-    public int maxVowels(String s, int k) 
-    {
-        char[]str=s.toCharArray();
-        String vowel="aeiou";
-        int c=0;
-        int max=0;
+class Solution 
+{
+    public int maxVowels(String s, int k) {
+       
+        char[] str = s.toCharArray();
+        
+        String vowel = "aeiou";
+        
+     
+        int c = 0;
+        
+        int max = 0;
 
-        for(int i=0;i<k;i++)
-        {
-            if(vowel.indexOf(str[i])!=-1)
-            {
+        // Count the number of vowels in the first window of size k
+        for (int i = 0; i < k; i++) {
+           
+            if (vowel.indexOf(str[i]) != -1) {
                 c++;
             }
         }
-        max=c;
-        int j=0;
+        // Initialize max with the vowel count of the first window
+        max = c;
+        
+        // Start a pointer to the beginning of the sliding window
+        int j = 0;
 
-        for(int i=k;i<str.length;i++)
-        { 
-            if(vowel.indexOf(str[i])!=-1)
-            {
-                c++;
+        for (int i = k; i < str.length; i++) { 
+           
+            if (vowel.indexOf(str[i]) != -1) {
+                c++; 
             }
-            if(vowel.indexOf(str[j])!=-1)
-            {
-                c--;
+            // Check if the character exiting the window (at index j) is a vowel
+            if (vowel.indexOf(str[j]) != -1) {
+                c--; 
             }
-            j++;
-            max=Math.max(max,c);
+            j++; // Move the start of the window forward
 
+          
+            max = Math.max(max, c);
         }
+        
+  
         return max;
-
-
     }
 }
