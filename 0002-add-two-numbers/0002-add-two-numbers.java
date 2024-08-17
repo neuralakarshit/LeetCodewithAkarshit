c821ac7961e5d2c7caa908cb278559a40dbbd739
Akarshit Srivastava <161750326+neuralakarshit@users.noreply.github.com>
2024-08-17 11:50:24 +0530

    Time: 1 ms (100%), Space: 45 MB (5.51%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -1,59 +1,55 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution 
 {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) 
     {
-
-        if(l1==null)
+        // If l1 is null, return l2 because there's nothing to add from l1.
+        if (l1 == null) 
         {
             return l2;
-        }
-        else if(l2==null)
+        } 
+        // If l2 is null, return l1 because there's nothing to add from l2.
+        else if (l2 == null) 
         {
             return l1;
         }
 
-        ListNode temp=new ListNode(0);
-        ListNode head=temp;
-        int carry=0;
+        // Create a dummy node to store the result and initialize 'temp' to this dummy node.
+        ListNode temp = new ListNode(0);
+        // 'head' will be used to return the result, pointing to the dummy node initially.
+        ListNode head = temp;
+        // Initialize carry to 0, which will hold any carryover from addition.
+        int carry = 0;
 
-       while(l1!=null || l2!=null)
-       {
-          if(l1!=null)
-          {
-             carry+=l1.val;
-             l1=l1.next;
+        // Loop until both l1 and l2 are fully processed.
+        while (l1 != null || l2 != null) 
+        {
+            // If l1 is not null, add its value to carry and move to the next node.
+            if (l1 != null) {
+                carry += l1.val;
+                l1 = l1.next;
+            }
+            // If l2 is not null, add its value to carry and move to the next node.
+            if (l2 != null) {
+                carry += l2.val;
+                l2 = l2.next;
+            }
 
-          }
-          if(l2!=null)
-          {
-            carry+=l2.val;
-            l2=l2.next;
-          }
+            // Create a new node with the digit part of carry (carry % 10) and link it to temp.
+            temp.next = new ListNode(carry % 10);
+            // Move temp to the next node to continue building the result list.
+            temp = temp.next;
 
-          temp.next =new ListNode(carry%10);
-          temp=temp.next;
+        // Update carry with the carryover part (carry / 10) to be added in the next iteration.
+            carry = carry / 10;
+        }
 
-          carry=carry/10;
-       }
+        // After the loop, if there's any remaining carry, create a new node for it.
+        if (carry > 0) 
+        {
+            temp.next = new ListNode(carry);
+        }
 
-          if(carry>0)
-          {
-            temp.next=new ListNode(carry);
-
-          }
-
-          return head.next;
-
-        
+        // Return the next node of the dummy node, which is the head of the result list.
+        return head.next;
     }
 }
