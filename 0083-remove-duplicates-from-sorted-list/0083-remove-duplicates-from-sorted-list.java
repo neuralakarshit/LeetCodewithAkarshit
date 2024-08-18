fe386688e55ea24382ab49c1398b4a5e193ca4f7
Akarshit Srivastava <161750326+neuralakarshit@users.noreply.github.com>
2024-08-18 22:39:06 +0530

    Time: 0 ms (100%), Space: 44 MB (55.19%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
@@ -8,7 +8,11 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
+
+ /*       --> ALTERNATE APPROACH
+
+class Solution 
+{
     public ListNode deleteDuplicates(ListNode head) 
     {
         if(head==null || head.next==null)
@@ -32,6 +36,37 @@ class Solution {
                 prev=temp;
                 temp=temp.next;
             }
+        }
+        return head;
+    }
+}
+
+  */
+
+class Solution 
+{
+    public ListNode deleteDuplicates(ListNode head) 
+    {
+        if(head==null || head.next==null)
+        {
+            return head;
+        }
+
+        ListNode temp=head;
+     
+
+        while(temp!=null && temp.next!=null)
+        {
+            if(temp.val==temp.next.val)
+            {
+                temp.next=temp.next.next;
+         
+            }
+            else
+            {
+                temp=temp.next;
+            }
+            
         }
         return head;
     }
