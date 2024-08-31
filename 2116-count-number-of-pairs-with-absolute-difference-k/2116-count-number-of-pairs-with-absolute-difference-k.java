ee5111e150d10bdae514dbbe759087a9b1d64615
Akarshit Srivastava <161750326+neuralakarshit@users.noreply.github.com>
2024-09-01 04:00:48 +0530

    Time: 5 ms (72.19%), Space: 44.1 MB (15.26%) - LeetHub

--- 2116-count-number-of-pairs-with-absolute-difference-k/2116-count-number-of-pairs-with-absolute-difference-k.java
+++ 2116-count-number-of-pairs-with-absolute-difference-k/2116-count-number-of-pairs-with-absolute-difference-k.java
@@ -7,20 +7,24 @@ class Solution
         int count = 0;
 
             // Iterate through each element in the array
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < nums.length; i++) 
+        {
             int num = nums[i];
+             map.put(num, map.getOrDefault(num, 0) + 1);
 
             // Check if (num - k) exists in the map
-            if (map.containsKey(num - k)) {
+            if (map.containsKey(num - k)) 
+            {
                 count += map.get(num - k);
             }
             // Check if (num + k) exists in the map
-            if (map.containsKey(num + k)) {
+            if (map.containsKey(num + k)) 
+            {
                 count += map.get(num + k);
             }
 
             // Update the map with the count of the current number
-            map.put(num, map.getOrDefault(num, 0) + 1);
+           // map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
         return count;
