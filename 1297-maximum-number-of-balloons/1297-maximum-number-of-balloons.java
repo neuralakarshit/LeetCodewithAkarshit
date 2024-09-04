8dbf76da9f8785b93d23298abd199ac9c1f91dd2
Akarshit Srivastava <161750326+neuralakarshit@users.noreply.github.com>
2024-09-04 23:32:47 +0530

    Time: 6 ms (57.17%), Space: 42.3 MB (32.21%) - LeetHub

--- 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.java
+++ 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.java
@@ -6,19 +6,31 @@ class Solution
         HashMap<Character, Integer> countMap = new HashMap<>();
 
         // Count the frequency of each character in the input string
-        for (char c : text.toCharArray()) {
-            if (c == 'b' || c == 'a' || c == 'l' || c == 'o' || c == 'n') {
+        for (char c : text.toCharArray()) 
+        {
+            if (c == 'b' || c == 'a' || c == 'l' || c == 'o' || c == 'n') 
+            {
                 countMap.put(c, countMap.getOrDefault(c, 0) + 1);
             }
+
         }
 
         // Adjust the counts for 'l' and 'o' (which appear twice in "balloon")
         countMap.put('l', countMap.getOrDefault('l', 0) / 2);
         countMap.put('o', countMap.getOrDefault('o', 0) / 2);
 
+
+
         // Find the minimum count among 'b', 'a', 'l', 'o', 'n'
-return Math.min(Math.min(countMap.getOrDefault('b', 0), countMap.getOrDefault('a', 0)),Math.min(countMap.getOrDefault('l', 0), Math.min(countMap.getOrDefault('o', 0), countMap.getOrDefault('n', 0))));
-    
+        int minBalloons = Integer.MAX_VALUE;
+        for (char c : "balon".toCharArray()) 
+        {
+            minBalloons = Math.min(minBalloons, countMap.getOrDefault(c,0));
+        }
+        
+        return minBalloons;
+
+       
     
     }
 }
