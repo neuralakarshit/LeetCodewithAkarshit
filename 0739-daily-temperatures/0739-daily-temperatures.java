e61fe144a7c2385d58e071e669d45d96fb61ff9a
Akarshit Srivastava <161750326+neuralakarshit@users.noreply.github.com>
2024-09-10 00:18:07 +0530

    Time: 81 ms (19%), Space: 60.1 MB (69.73%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,3 +1,25 @@
+/*
+
+       ----> Alternate way<-----
+
+       int[] result = new int[T.length];
+       Stack<Integer> stack = new Stack<>();
+        
+        for (int i = 0; i < T.length; i++)
+        {
+            while (!stack.isEmpty() && T[i] > T[stack.peek()]) 
+            {
+                int idx = stack.pop();
+                result[idx] = i - idx;  // Calculate the number of days
+            }
+            stack.push(i);  // Push the current index onto the stack
+        }
+        
+        return result;
+
+*/
+
+
 class Solution 
 {
     public int[] dailyTemperatures(int[] temperatures)
