2310098a84849e02b9c9c2d108968aa346b9d2d2
Akarshit Srivastava <161750326+neuralakarshit@users.noreply.github.com>
2025-01-14 19:59:59 +0530

    Time: 0 ms (100%), Space: 45.2 MB (79.56%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,36 +1,19 @@
-class Solution {
+class Solution 
+{
     public int missingNumber(int[] nums)
     
-     {
+    {
+        int n=nums.length;
+        int TotalSum=(n*(n+1))/2;
+     
 
-        int n= nums.length;
-        int TotalSum=n*(n+1)/2;
-        int actualSum=0;
-        boolean c=false;
-        int missingNum=-1;
-        for(int i=0;i<n;i++)
+        for(int i=0;i<nums.length;i++)
         {
-            if(nums[i]==0)
-            {
-                c=true;   // to counter number 0 presence!
-            }
+            TotalSum-=nums[i];
 
         }
+        return TotalSum;
 
-        if(c)
-        {
-            for(int i=0;i<n;i++)
-            {
-                actualSum+=nums[i];  
-            }
-            missingNum=TotalSum-actualSum;
-            
-        }
-        else
-        {
-            missingNum=0;
-        }
-        return missingNum;
-        
+       
     }
 }
